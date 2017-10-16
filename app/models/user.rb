b50eb847ea8c6803192d3fa944efeb5db8c5e5bd
dag <gerardoayalad@outlook.com>
2017-10-16 00:44:47 -0500

    First Commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :confirmable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+end
